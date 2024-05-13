3a8d83af38b771883362b2e49b35b93551a9a773
aminbabu12 <badrulaminbabu@gmail.com>
2024-05-12 20:44:15 -0400

    faq section included

--- main.js
+++ main.js
@@ -3,3 +3,21 @@
 window.addEventListener('scroll' , () =>{
   document.querySelector('nav').classList.toggle('window-scroll' , window.scrollY > 0)
 })
+
+//show and hide faq's asnwer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click' , () => {
+    faq.classList.toggle('open');
+
+    //change icon
+    const icon = faq.querySelector('.faq-icon i');
+    if(icon.className === 'fa-solid fa-plus'){
+      icon.className = "fa-solid fa-minus"
+    } else{
+      icon.className = "fa-solid fa-plus";
+    }
+  })
+})
